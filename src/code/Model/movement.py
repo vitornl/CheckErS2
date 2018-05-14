513c6d800b4e556bb595428fdadb3ef2c9daeae3
VitorA29 <vitor_araujo.29@outlook.com>
2018-05-14 16:48:48 -0300

    Reworked Movement class

--- src/code/Model/movement.py
+++ src/code/Model/movement.py
@@ -1,16 +1,78 @@
 class Movement:
-    def __init__(self, position, location_eliminated_piece):
-        self.position = position
-        self.location_eliminated_piece = location_eliminated_piece
+    """
+        Class that deals with the movement routing
+    """
+    def __init__(self, piece, destiny):
+        """
+            Class builder
 
-    def get_position(self):
-        return self.position
+            Parameters
+            ----------
+            piece: Piece that is going to move
+            destiny: New position of the piece
 
-    def set_position(self, position):
-        self.position = position
+            Returns
+            -------
+            A Movement class object
+        """
+        self.piece = piece
+        self.destiny = []
+        self.destiny.append(destiny)
+        self.eliminated = []
 
-    def get_location_eliminated_piece(self):
-        return self.location_eliminated_piece
+    def get_piece(self):
+        """
+            Getter for piece
 
-    def set_location_eliminated_piece(self, location_piece):
-        self.location_eliminated_piece = location_piece
+            Returns
+            -------
+            The piece that is in movement
+        """
+        return self.piece
+
+    def get_movement(self):
+        """
+            Getter for first movement
+
+            Returns
+            -------
+            The destiny position
+        """
+        return self.destiny.pop()
+
+    def get_eliminateds(self):
+        """
+            Getter for eliminated pieces list
+
+            Returns
+            -------
+            The list of eliminated pieces
+        """
+        return self.eliminated
+
+    def add_new_destiny(self, location):
+        """
+            Add a new destiny for the piece
+
+            Parameters
+            ----------
+            location: New position of the piece
+
+            Returns
+            -------
+            Bool meaning if is a new position or not
+        """
+        if location in self.destiny:
+            return False
+        self.destiny.append(location)
+        return True
+
+    def add_new_elimination(self, piece):
+        """
+            Add a new eliminated piece
+
+            Parameters
+            ----------
+            piece: New piece to be removed
+        """
+        self.eliminated.append(piece)
