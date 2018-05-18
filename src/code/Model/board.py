f1cac18d4ed79b43b8ca9844aabe14c39f5d430d
VitorA29 <vitor_araujo.29@outlook.com>
2018-05-18 20:50:44 -0300

    Added in board model

--- src/code/Model/board.py
+++ src/code/Model/board.py
@@ -1,40 +1,80 @@
 class Board:
+    """
+        Class for dealing with the board
+
+        Parameters
+        ----------
+        board: game board, matrix where each element is None or a piece
+    """
     def __init__(self):
+        """
+            Class builder
+
+            Returns
+            -------
+            A Board class object
+        """
         self.board = self._new_board()
 
     def _new_board(self):
+        """
+            Start-up a new board
+
+            Returns
+            -------
+            A matrix where every None
+        """
         board = [[None] * 8 for i in range(8)]
         
         return board
 
     def get_piece(self, position):
+        """
+            Getter for a piece
+
+            Parameters
+            ----------
+            position: position of the expected piece
+
+            Returns
+            -------
+            A piece or None
+        """
         if position[0]<0 or position[0]>7 or position[1]<0 or position[1]>7:
             return None
         return self.board[position[0]][position[1]]
 
-    def get_piece_surroundings(self, piece):
-        resp = []
-        
-        x, y = piece.position
-        if x > 0 and y > 0:
-            resp.append(self.board[x-1][y-1])
-        if x < 7 and y > 0:
-            resp.append(self.board[x+1][y-1])
-        if x > 0 and y < 7:
-            resp.append(self.board[x-1][y+1])
-        if x < 7 and y < 7:
-            resp.append(self.board[x+1][y+1])
-        
-        return resp
-
     def add_piece(self, piece, position):
+        """
+            Add a piece in the board
+
+            Parameters
+            ----------
+            piece: the piece that is going to be placed in the board
+            position: position to place the piece
+        """
         self.board[position[0]][position[1]] = piece
         piece.set_position(position)
 
     def move_piece(self, piece, position):
+        """
+            Move a piece in the board
+
+            Parameters
+            ----------
+            piece: the piece that is going to be moved in the board
+            position: position to place the piece
+        """
         self.remove_piece(piece)
         self.add_piece(piece, position)
 
     def remove_piece(self, piece):
+        """
+            Remove a piece from the board
+
+            Parameters
+            ----------
+            piece: the piece that is going to be removed from the board
+        """
         self.board[piece.position[0]][piece.position[1]] = None
         piece.set_position((None, None))
