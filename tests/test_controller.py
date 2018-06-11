84c2f1f9546c9e0e73e4cf722712e4a7ffb33fa2
Leonardo Schimpf Machado <leonardo.schimpf@gmail.com>
2018-06-11 11:30:19 -0300

    Incrementando um pouco os testes unitários de controller...

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -1,7 +1,10 @@
 from src.mvc.Controller.rules import Rule
+from src.mvc.Model.bot import Bot
+from src.mvc.Model.player import Player
+from src.mvc.Model.piece import Piece
+from src.mvc.Model.board import Board
 from unittest.mock import Mock, MagicMock, patch
 import pytest
-import mock
 
 class TestController:
 # Testes do Controller
@@ -44,3 +47,35 @@ class TestController:
         assert rules.players[0].get_qty_draughts() == 1
         assert rules.players[1].get_qty_draughts() == 1
         file.close()
+
+    def test_rules_set_players(self):
+        rules = Rule('human')
+        (p1, p2), turn_player = rules._set_players('bot')
+        assert type(p1) == Bot or type(p2) == Bot
+        (p1, p2), turn_player = rules._set_players('human')
+        assert type(p1) == Player and type(p2) == Player
+
+    def test_rules_init_board(self):
+        rules = Rule('human')
+        rules.board = Board()
+        rules._init_board(rules.board, rules.players)
+        for i in range(0, 8):
+            for j in range(0, 8):
+                if i not in (3, 4) and (j + i) % 2 != 0:
+                    assert type(rules.board.board[j][i]) == Piece
+                else:
+                    assert rules.board.board[j][i] is None
+
+    def test_rules_evaluate_position(self):
+        rules = Rule('human')
+        assert rules._evaluate_position((-1, 0), Piece("p1")) == 0
+        assert rules._evaluate_position((8, 0), Piece("p1")) == 0
+        assert rules._evaluate_position((0, -1), Piece("p1")) == 0
+        assert rules._evaluate_position((0, 8), Piece("p1")) == 0
+        assert rules._evaluate_position((0, 0), rules.players[0].pieces[0]) == 1
+        assert rules._evaluate_position((1, 0), rules.players[0].pieces[0]) == 0
+        assert rules._evaluate_position((0, 5), rules.players[0].pieces[0]) == 2
+
+    def test_rules_build_eating_path(self):
+
+
